289a3d5521b514030ad9ad8b56aac2b6362500ab
zmdmths <sksvlshzldh2@naver.com>
2024-06-11 22:11:54 +0900

    update

---
+++ pi/car.py
@@ -0,0 +1,32 @@
+import pi.car as car
+import datetime
+import time
+import csv
+import pi.define as de
+
+def can_net():
+   bus = car.interface.Bus(channel='can0', bustype='socketcan')
+   file_counter = 1
+   file = de.open_new_file(file_counter)
+   start_time = time.time()
+   writer = csv.DictWriter(file, fieldnames=de.fieldnames)
+   writer.writeheader()
+
+   try:
+         while True:
+            if time.time() - start_time >= 30:
+               file.close()
+               file_counter+=1
+               file = de.open_new_file(file_counter)
+               start_time = time.time()
+               writer = csv.DictWriter(file, fieldnames=de.fieldnames)
+               writer.writeheader()
+            message=bus.recv()
+            data = int.from_bytes(message.data, byteorder='big', signed=True) #비트 연산을 위해 정수 변환
+            if message.arbitration_id in [de.EMS11, de.EMS12, de.ESP12, de.TCU12, de.SAS11]:
+               csv_writer = csv.writer(file)
+               de.process_data(message, data, file)
+               file.flush()
+
+   finally:
+      file.close()
